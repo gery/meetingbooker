12075c61f4c3a26feacd89f6312878dc9256d004
gery <gary.europe@gmail.com>
2023-09-11 15:16:19 +0200

    Update MeetingApplicationTests.java

--- src/test/java/com/chatlord/meeting/MeetingApplicationTests.java
+++ src/test/java/com/chatlord/meeting/MeetingApplicationTests.java
@@ -6,17 +6,11 @@ import com.chatlord.meeting.model.Meeting;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import java.util.List;
 import java.util.Collection;
-
-import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -52,16 +46,7 @@ class MeetingApplicationTests {
 		meetings = meetingRepository.findAll();
 		assertThat(meetings.size()).isEqualTo(1);
 		assertThat(meetings.stream().findFirst().get().getName()).isEqualTo("Joe");
-/* 
-		this.mockMvc.perform(
-				post("/meeting")
-						.contentType("application/json")
-						.content("{\"name\": \"joe\", \"date\": \"2023-12-12\", \"timeFrom\": \"11:30\"}"))
-				.andExpect(status().isBadRequest());
 
-		meetings = meetingRepository.findAll();
-		assertThat(meetings.size()).isEqualTo(1);
-		*/
 	}
 
 
